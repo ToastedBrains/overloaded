709fba781cdff3b11e48f6286907a4c197b11eea
gil-x <gino.ladowitch@gmail.com>
2024-01-13 15:19:49 +0100

    Player basics.

---
+++ src/player/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+const SPEED = 600
+
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * SPEED
+	Debug.print(direction)
+	look_at(global_position + direction)
+	move_and_slide()
